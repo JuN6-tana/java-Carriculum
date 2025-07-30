1887abfec468955d1d4138290b389dbb095f576a
junha <junha@LAPTOP-CBECL6D3>
2025-07-30 22:17:11 +0900

    4,5

--- HelloWorld_TanakaJun/src/curriculum_B/Qes5.java
+++ HelloWorld_TanakaJun/src/curriculum_B/Qes5.java
@@ -4,7 +4,22 @@ public class Qes5 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 1; j <= 20; j++) {
+				if (j < 20) {
+					System.out.print(" " + String.format("%03d", j) + "*" + String.format("%03d", i) + "=" + String.format("%03d", i * j) + " || ");
+						
+				} else {
+					System.out.print(" " + String.format("%03d", j) + "*" + String.format("%03d", i) + "=" + String.format("%03d", i * j));
+				}
+				
+				
+				
+			}
+			
+			System.out.println(); 
+			
+		}	
 	}
 
 }
